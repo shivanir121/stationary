54a6dd9381f93b9b6234fc5d18f0a55da6286e36
SHIVANI RAJPUT <shivanir121@gmail.com>
2020-02-24 16:42:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+$('#carouselExampleIndicators').carousel({
+    interval: 2000
+})
+
+$('#carouselExample').carousel({
+    interval: 2000
+})
+
+
+sr.reveal('.leftAnimation', {
+    origin: 'left',
+});
+sr.reveal('.RightAnimation , .showcase', {
+    origin: 'right',
+});
+
+sr.reveal('.bottomAnimation', {
+    origin: 'bottom',
+});
+
+
